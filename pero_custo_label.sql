dba923d445d373715c9c099699a219e184edb8d3
lao-natalie <89660903+lao-natalie@users.noreply.github.com>
2022-01-27 10:24:21 -0500

    Update pero_custo_label.sql

--- pero_custo_label.sql
+++ pero_custo_label.sql
@@ -1,3 +1,4 @@
+-- birth stone/flower
 CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.nlao.perso_custo_listing_label` AS (
 WITH
   pero_custo_label AS (
@@ -11,7 +12,6 @@ WITH
         OR LOWER(title) LIKE '%monogram%' 
         OR (LOWER(title) LIKE '%custom%' AND LOWER(title) NOT LIKE '%customer%') 
         OR LOWER(title) LIKE '%made to order%' OR LOWER(title) LIKE '%made-to-order%' 
-        OR LOWER(title) LIKE '%handmade%' OR LOWER(title) LIKE '%hand-made%' OR LOWER(title) LIKE '%hand made%'
         THEN 1
       ELSE 0
     END AS title_text_custom,
@@ -19,8 +19,7 @@ WITH
         (LOWER(description) LIKE '%personali%' AND LOWER(description) NOT LIKE '%personality%')
         OR LOWER(description) LIKE '%monogram%'
         OR (LOWER(description) LIKE '%custom%' AND LOWER(description) NOT LIKE '%customer%')
-        OR LOWER(description) LIKE '%made to order%' OR LOWER(description) LIKE '%made-to-order%'
-        OR LOWER(description) LIKE '%handmade%' OR LOWER(description) LIKE '%hand-made%' OR LOWER(description) LIKE '%hand made%' 
+        OR LOWER(description) LIKE '%made to order%' OR LOWER(description) LIKE '%made-to-order%' 
         THEN 1
       ELSE 0
     END AS description_text_custom,
@@ -43,60 +42,78 @@ WITH
 )
 
 SELECT
-  current_date() as run_date,
-  pc.listing_id,
-  pc.shop_id,
-  pc.title,
-  pc.description,
-  pc.title_text_custom,
-  pc.description_text_custom,
-  pc.attribute_personalizable,
+  listing_id,
+  shop_id,
+  title,
+  description,
   CASE WHEN 
-        REGEXP_CONTAINS(LOWER(pc.title), r'birth flower|birthflower|birth month flower|birthmonth flower') OR
-        REGEXP_CONTAINS(LOWER(pc.attribute_name), r'birth flower|birthflower|birth month flower|birthmonth flower')
+        REGEXP_CONTAINS(LOWER(title), r'birth flower|birthflower|birth month flower|birthmonth flower') OR
+        REGEXP_CONTAINS(LOWER(attribute_name), r'birth flower|birthflower|birth month flower|birthmonth flower')
       THEN 1
       ELSE 0
     END AS birth_flower,
   CASE WHEN 
-        REGEXP_CONTAINS(LOWER(pc.title), r'birth stone|birthstone|birth month stone|birthmonth stone') OR
-        REGEXP_CONTAINS(LOWER(pc.attribute_name), r'birth stone|birthstone|birth month stone|birthmonth stone')
+        REGEXP_CONTAINS(LOWER(title), r'birth stone|birthstone|birth month stone|birthmonth stone') OR
+        REGEXP_CONTAINS(LOWER(attribute_name), r'birth stone|birthstone|birth month stone|birthmonth stone')
       THEN 1
       ELSE 0
     END AS birth_stone,  
   CASE WHEN 
-        REGEXP_CONTAINS(LOWER(pc.title), r'zodiac|astrology|constellation|star sign') OR
-        REGEXP_CONTAINS(LOWER(pc.attribute_name), r'zodiac|astrology|constellation|star sign') OR
-        (REGEXP_CONTAINS(LOWER(pc.attribute_value), r'sagittarius') AND REGEXP_CONTAINS(LOWER(pc.attribute_value), r'taurus'))
+        REGEXP_CONTAINS(LOWER(title), r'zodiac|astrology|constellation|star sign') OR
+        REGEXP_CONTAINS(LOWER(attribute_name), r'zodiac|astrology|constellation|star sign') OR
+        (REGEXP_CONTAINS(LOWER(attribute_value), r'sagittarius') AND REGEXP_CONTAINS(LOWER(attribute_value), r'taurus'))
       THEN 1
       ELSE 0
     END AS zodiac_sign,
   CASE WHEN 
-       (pc.title_text_custom = 1 OR pc.description_text_custom = 1) AND
-       ((REGEXP_CONTAINS(LOWER(pc.title), r'state|country|region') OR REGEXP_CONTAINS(LOWER(attribute_name), r'state|country|region')) OR
-       (LOWER(pc.title) LIKE "city %" OR LOWER(pc.title) LIKE "city%" OR LOWER(pc.title) LIKE "% city" OR LOWER(pc.title) LIKE "% city %" OR LOWER(pc.title) LIKE "city") OR
-       (LOWER(pc.attribute_name) LIKE "city %" OR LOWER(pc.attribute_name) LIKE "city%" OR LOWER(pc.attribute_name) LIKE "% city" OR LOWER(pc.attribute_name) LIKE "% city %" OR LOWER(pc.attribute_name) LIKE "city") OR
-       (LOWER(pc.attribute_name) LIKE "state %" OR LOWER(pc.attribute_name) LIKE "state%" OR LOWER(pc.attribute_name) LIKE "% state" OR LOWER(pc.attribute_name) LIKE "% state %" OR LOWER(pc.attribute_name) LIKE "state") OR
-       (LOWER(pc.attribute_name) LIKE "country %" OR LOWER(pc.attribute_name) LIKE "country%" OR LOWER(pc.attribute_name) LIKE "% country" OR LOWER(pc.attribute_name) LIKE "% country %" OR LOWER(pc.attribute_name) LIKE "country") OR
-       (LOWER(pc.attribute_name) LIKE "region %" OR LOWER(pc.attribute_name) LIKE "region%" OR LOWER(pc.attribute_name) LIKE "% region" OR LOWER(pc.attribute_name) LIKE "% region %" OR LOWER(pc.attribute_name) LIKE "region"))
+       (title_text_custom = 1 OR description_text_custom = 1) AND
+       ((REGEXP_CONTAINS(LOWER(title), r'state|country|region') OR REGEXP_CONTAINS(LOWER(attribute_name), r'state|country|region')) OR
+       (LOWER(title) LIKE "city %" OR LOWER(title) LIKE "city%" OR LOWER(title) LIKE "% city" OR LOWER(title) LIKE "% city %" OR LOWER(title) LIKE "city") OR
+       (LOWER(attribute_name) LIKE "city %" OR LOWER(attribute_name) LIKE "city%" OR LOWER(attribute_name) LIKE "% city" OR LOWER(attribute_name) LIKE "% city %" OR LOWER(attribute_name) LIKE "city") OR
+       (LOWER(attribute_name) LIKE "state %" OR LOWER(attribute_name) LIKE "state%" OR LOWER(attribute_name) LIKE "% state" OR LOWER(attribute_name) LIKE "% state %" OR LOWER(attribute_name) LIKE "state") OR
+       (LOWER(attribute_name) LIKE "country %" OR LOWER(attribute_name) LIKE "country%" OR LOWER(attribute_name) LIKE "% country" OR LOWER(attribute_name) LIKE "% country %" OR LOWER(attribute_name) LIKE "country") OR
+       (LOWER(attribute_name) LIKE "region %" OR LOWER(attribute_name) LIKE "region%" OR LOWER(attribute_name) LIKE "% region" OR LOWER(attribute_name) LIKE "% region %" OR LOWER(attribute_name) LIKE "region"))
       THEN 1
       ELSE 0
     END AS location,
   CASE WHEN
-        (pc.title_text_custom = 1 OR pc.description_text_custom = 1) AND
-        (REGEXP_CONTAINS(LOWER(pc.title), r'alphabet|letter|initial') OR
-        REGEXP_CONTAINS(LOWER(pc.attribute_name), r'alphabet|letter|initial')) 
+        (title_text_custom = 1 OR description_text_custom = 1) AND
+        (REGEXP_CONTAINS(LOWER(title), r'alphabet|letter|initial|character') OR
+        REGEXP_CONTAINS(LOWER(attribute_name), r'alphabet|letter|initial|character')) 
         THEN 1
       ELSE 0
     END AS initial,
   CASE WHEN
-        (pc.title_text_custom = 1 OR pc.description_text_custom = 1) AND
-        (REGEXP_CONTAINS(LOWER(pc.title), r'moon phase|moonphase') OR
-        REGEXP_CONTAINS(LOWER(pc.attribute_name), r'moon phase|moonphase') OR 
-        LOWER(pc.attribute_value) LIKE "%waxing gibbous%")
+        (title_text_custom = 1 OR description_text_custom = 1) AND
+        (REGEXP_CONTAINS(LOWER(title), r'moon phase') OR REGEXP_CONTAINS(LOWER(title), r'moonphase') OR
+        REGEXP_CONTAINS(LOWER(attribute_name), r'moon phase') OR REGEXP_CONTAINS(LOWER(attribute_name), r'moonphase') OR 
+        LOWER(attribute_value) LIKE "%waxing gibbous%")
         THEN 1
       ELSE 0
-    END AS moon_phase
-FROM pero_custo_label pc
+    END AS moon_phase,
+  CASE WHEN
+        (title_text_custom = 1 OR description_text_custom = 1) AND
+        ((REGEXP_CONTAINS(LOWER(attribute_value), r'font|schriftart') AND LOWER(attribute_name) NOT LIKE "%color%" AND LOWER(attribute_name) NOT LIKE "%colour%") OR 
+        (REGEXP_CONTAINS(LOWER(attribute_value), r'arial|times new roman|comic sans')))
+        THEN 1
+      ELSE 0
+    END AS font,
+  CASE WHEN
+        (title_text_custom = 1 OR description_text_custom = 1) AND
+        (LOWER(attribute_name) LIKE "%shape%")
+        THEN 1
+      ELSE 0
+    END AS shape,
+   CASE WHEN
+        (title_text_custom = 1 OR description_text_custom = 1) AND
+        (LOWER(attribute_name) LIKE "image" OR LOWER(attribute_name) LIKE "%icon%")
+        THEN 1
+      ELSE 0
+    END AS image_icon    
+FROM pero_custo_label
 )
+
+
+
 
 
