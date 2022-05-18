84bc72fbf9bb68decb72870d3e0fab7837d06783
lao-natalie <89660903+lao-natalie@users.noreply.github.com>
2022-05-18 09:52:56 -0400

    Update uniqueness.sql

--- uniqueness.sql
+++ uniqueness.sql
@@ -1,3 +1,4 @@
+# CREATE TABLE THAT CONSOLIDATE ALL KB CONCEPTS
 CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.nlao.listing_concepts_uniqueness` AS (
 WITH listing_concepts AS (
 SELECT 
@@ -63,7 +64,15 @@ lc.*,
 r.concept_percentile,
 r.concept_vigintile,
 r.concept_decile,
-r.concept_quintile
+r.concept_quintile,
+AVG(concept_percentile) OVER (PARTITION BY listing_id) AS avg_percentile,
+AVG(concept_vigintile) OVER (PARTITION BY listing_id) AS avg_vigintile,
+AVG(concept_decile) OVER (PARTITION BY listing_id) AS avg_decile,
+AVG(concept_quintile) OVER (PARTITION BY listing_id) AS avg_quintile,
+PERCENTILE_CONT(concept_percentile, 0.5) OVER (PARTITION BY listing_id) AS med_percentile,
+PERCENTILE_CONT(concept_vigintile, 0.5) OVER (PARTITION BY listing_id) AS med_vigintile,
+PERCENTILE_CONT(concept_decile, 0.5) OVER (PARTITION BY listing_id) AS med_decile,
+PERCENTILE_CONT(concept_quintile, 0.5) OVER (PARTITION BY listing_id) AS med_quintile
 FROM active_listings lc
 JOIN rareness r ON lc.concept_domain = r.concept_domain AND lc.value = r.value
 )
@@ -78,10 +87,47 @@ GROUP BY 1
 ORDER BY 2 DESC
 ;
 
--- CONCEPT UNIQUENESS
+--CONCEPT UNIQUENESS
 SELECT
-distinct concept_domain,
+concept_domain,
 value,
-concept_percentile
-FROM `etsy-data-warehouse-dev.nlao.listing_concepts_uniqueness` 
+concept_percentile,
+concept_vigintile,
+concept_decile,
+concept_quintile
+FROM `etsy-data-warehouse-dev.nlao.listing_concepts_uniqueness`
 ;
+
+-- HYPO 1: AVG PERCENTILE
+SELECT
+ROUND(avg_percentile,0) AS round_avg_percentile,
+COUNT(DISTINCT value) AS concept_count,
+COUNT(DISTINCT listing_id) AS listing_count
+FROM `etsy-data-warehouse-dev.nlao.listing_concepts_uniqueness`
+GROUP BY 1
+ORDER BY 1 ASC
+;
+
+-- HYPO 1: TAKE SAMPLE
+SELECT listing_id FROM `etsy-data-warehouse-dev.nlao.listing_concepts_uniqueness` WHERE ROUND(avg_percentile,0) >= 1 limit 50;
+SELECT listing_id FROM `etsy-data-warehouse-dev.nlao.listing_concepts_uniqueness` WHERE ROUND(avg_percentile,0) <= 75 limit 50;
+
+-- HYPO 2: MEDIAN PERCENTILE
+SELECT
+ROUND(med_percentile,0) AS round_med_percentile,
+COUNT(DISTINCT listing_id) AS listing_count
+FROM `etsy-data-warehouse-dev.nlao.listing_concepts_uniqueness`
+GROUP BY 1
+ORDER BY 1 ASC
+;
+
+-- HYPO 2: TAKE SAMPLE
+SELECT listing_id FROM `etsy-data-warehouse-dev.nlao.listing_concepts_uniqueness` WHERE ROUND(med_percentile,0) >= 1 limit 50;
+SELECT listing_id FROM `etsy-data-warehouse-dev.nlao.listing_concepts_uniqueness` WHERE ROUND(med_percentile,0) <= 75 limit 50;
+
+
+
+
+
+
+
