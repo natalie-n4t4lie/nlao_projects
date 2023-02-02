8b020d456060b194a792109a1b71ab6594055326
lao-natalie <89660903+lao-natalie@users.noreply.github.com>
2023-02-02 13:43:12 -0500

    Update query_interest_baseline_analysis.sql

--- query_interest_baseline_analysis.sql
+++ query_interest_baseline_analysis.sql
@@ -1,22 +1,4 @@
-SELECT
-COUNT(DISTINCT query)
-FROM `etsy-data-warehouse-dev.knowledge_base.query_interests` i
-WHERE _date >= '2023-01-09'
-;--4,053,069
-
-SELECT
-COUNT(DISTINCT query)
-FROM `etsy-data-warehouse-dev.knowledge_base.query_interests_adhoc`
-WHERE _date <= '2023-01-20'
-;--161,369,291
-
-SELECT
-DISTINCT query 
-FROM `etsy-data-warehouse-dev.knowledge_base.query_interests_adhoc`
-WHERE _date <= '2023-01-20' AND query LIKE "%&#%"
-;
-
-
+-- REFORMAT HTML ESCAPE QUERIES --
 CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.nlao.query_interests` AS (
 SELECT
   REPLACE(REPLACE(query, '&quot;', '"'),"&#39;","'") AS clean_query,
@@ -24,41 +6,55 @@ SELECT
 FROM `etsy-data-warehouse-dev.knowledge_base.query_interests_adhoc` i
 WHERE _date <= '2023-01-20'
 )
-;--  &quot; -> "
- --  &#39; -> '
-
-SELECT
-DISTINCT clean_query 
-FROM `etsy-data-warehouse-dev.nlao.query_interests`
-WHERE clean_query LIKE "%&%"
 ;
 
+-- CHECK HOW MANY QUERY COVERAGE FOR HTML ESCAPE QUERIES
+select
+COUNT(DISTINCT CASE WHEN clean_query !=query THEN query ELSE NULL END) / count(DISTINCT query) AS html_queries_count_share,
+COUNT(CASE WHEN clean_query !=query THEN query ELSE NULL END) / count(query) AS html_query_prediction_count_share
+FROM `etsy-data-warehouse-dev.nlao.query_interests`
+;
+
+SELECT
+bin,
+COUNT(DISTINCT q.query) AS query,
+COUNT(DISTINCT CASE WHEN clean_query != qi.query THEN q.query else null end) AS html_query
+FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` q
+JOIN `etsy-data-warehouse-dev.nlao.query_interests` qi
+  ON q.query = qi.clean_query
+GROUP BY 1
+;
 
 -- BASIC STATS FOR QUERY BIN
 SELECT 
   bin,
   COUNT(DISTINCT query),
-  SUM(sessions),
   SUM(gms)/100 as gms
 FROM `etsy-data-warehouse-prod.rollups.query_level_metrics`
 GROUP BY 1
 ;
 
--- QUERY COUNT/SESSION/GMS COVERAGE
+-- === QUERY COUNT/GMS COVERAGE === --
+
 -- QUERY BIN
 SELECT 
   bin,
   COUNT(q.query) AS query_count,
   COUNT(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests`) THEN q.query ELSE NULL END) AS interest_query_count,
-  SUM(q.sessions) AS query_session_count,
-  SUM(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests`) THEN q.sessions ELSE NULL END) AS interest_query_sessions_count,
+  COUNT(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.1) THEN q.query ELSE NULL END) AS interest_01_query_count,
+    COUNT(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.15) THEN q.query ELSE NULL END) AS interest_015_query_count,
+    COUNT(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.2) THEN q.query ELSE NULL END) AS interest_02_query_count,
   SUM(gms)/100 AS query_gms,
   SUM(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests`) THEN gms ELSE NULL END)/100 AS interest_query_gms,
+  SUM(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.1) THEN gms ELSE NULL END)/100 AS interest_01_query_gms,
+  SUM(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.15) THEN gms ELSE NULL END)/100 AS interest_015_query_gms,
+  SUM(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.2) THEN gms ELSE NULL END)/100 AS interest_02_query_gms
 FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` q
 GROUP BY 1
 ;
 
--- QUERY INTENT TABLE DEDUP
+-- QUERY INTENT
+  -- QUERY INTENT TABLE DEDUP
 CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.nlao.query_intent_labels_dedupped` AS (
   WITH cte AS (
   SELECT
@@ -76,72 +72,176 @@ CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.nlao.query_intent_labels_dedupp
   WHERE rk = 1
 );
 
--- QUERY INTENT
 SELECT
   CASE WHEN confidence >= 0.5 THEN label ELSE 'Unsure' END AS label,
-  COUNT(l.query) AS query_count,
-  COUNT(CASE WHEN l.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests`) THEN l.query ELSE NULL END) AS interest_query_count,
-  SUM(l.sessions) AS query_session_count,
-  SUM(CASE WHEN l.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests`) THEN l.sessions ELSE NULL END) AS interest_query_sessions_count,
-  SUM(l.gms)/100 AS query_gms,
-  SUM(CASE WHEN l.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests`) THEN l.gms ELSE NULL END)/100 AS interest_query_gms
-FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` l
-LEFT JOIN `etsy-data-warehouse-dev.nlao.query_intent_labels_dedupped` q 
-  ON q.query_raw = l.query
+  COUNT(q.query) AS query_count,
+  COUNT(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests`) THEN q.query ELSE NULL END) AS interest_query_count,
+  COUNT(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.1) THEN q.query ELSE NULL END) AS interest_01_query_count,
+    COUNT(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.15) THEN q.query ELSE NULL END) AS interest_015_query_count,
+    COUNT(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.2) THEN q.query ELSE NULL END) AS interest_02_query_count,
+  SUM(gms)/100 AS query_gms,
+  SUM(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests`) THEN gms ELSE NULL END)/100 AS interest_query_gms,
+  SUM(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.1) THEN gms ELSE NULL END)/100 AS interest_01_query_gms,
+  SUM(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.15) THEN gms ELSE NULL END)/100 AS interest_015_query_gms,
+  SUM(CASE WHEN q.query IN (SELECT clean_query FROM `etsy-data-warehouse-dev.nlao.query_interests` WHERE score >=0.2) THEN gms ELSE NULL END)/100 AS interest_02_query_gms
+FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` q
+LEFT JOIN `etsy-data-warehouse-dev.nlao.query_intent_labels_dedupped` qi 
+  ON qi.query_raw = q.query
 GROUP BY 1
 ;
 
--- # OF INTEREST MAPPED TO QUERY
+-- === INTEREST LABEL COUNT=== --
+
+-- QUERY BIN
 WITH cte AS (
 SELECT
-  query,
-  COUNT(DISTINCT display_name) AS concept_count
+  clean_query,
+  COUNT(DISTINCT display_name) AS concept_count,
+  COUNT(DISTINCT CASE WHEN score >=0.1 THEN display_name ELSE NULL END) AS concept_01_count,
+  COUNT(DISTINCT CASE WHEN score >=0.15 THEN display_name ELSE NULL END) AS concept_015_count,
+  COUNT(DISTINCT CASE WHEN score >=0.2 THEN display_name ELSE NULL END) AS concept_02_count
 FROM `etsy-data-warehouse-dev.nlao.query_interests`
--- WHERE score >= 0.1
 GROUP BY 1
 )
 SELECT
   bin,
-  CASE WHEN concept_count IS NULL THEN 0 ELSE concept_count END AS concept_count,
-  COUNT(*) AS query_count,
-  SUM(l.sessions) AS query_session_count,
+  CASE WHEN concept_count IS NULL THEN 0 
+        WHEN concept_count >= 8 THEN 8 
+        ELSE concept_count END AS concept_count,
+  CASE WHEN concept_01_count IS NULL THEN 0 
+        WHEN concept_01_count >= 8 THEN 8 
+        ELSE concept_01_count END AS concept_01_count,
+  CASE WHEN concept_015_count IS NULL THEN 0 
+        WHEN concept_015_count >= 8 THEN 8 
+        ELSE concept_015_count END AS concept_015_count,
+  CASE WHEN concept_02_count IS NULL THEN 0 
+        WHEN concept_02_count >= 8 THEN 8 
+        ELSE concept_02_count END AS concept_02_count,
+  COUNT(query) AS query_count,
   SUM(l.gms)/100 AS query_gms,
 FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` l
 LEFT JOIN cte q 
-  ON q.query = l.query
-GROUP BY 1,2
+  ON q.clean_query = l.query
+GROUP BY 1,2,3,4,5
 ;
 
--- # OF INTEREST TYPE MAPPED TO QUERY
+-- QUERY INTENT
 WITH cte AS (
 SELECT
-  query,
-  COUNT(DISTINCT attribute_type) AS concept_type_count
+  clean_query,
+  COUNT(DISTINCT display_name) AS concept_count,
+  COUNT(DISTINCT CASE WHEN score >=0.1 THEN display_name ELSE NULL END) AS concept_01_count,
+  COUNT(DISTINCT CASE WHEN score >=0.15 THEN display_name ELSE NULL END) AS concept_015_count,
+  COUNT(DISTINCT CASE WHEN score >=0.2 THEN display_name ELSE NULL END) AS concept_02_count
 FROM `etsy-data-warehouse-dev.nlao.query_interests`
--- WHERE score >= 0.1
+GROUP BY 1
+)
+SELECT
+  CASE WHEN confidence >= 0.5 THEN label ELSE 'Unsure' END AS label,
+  CASE WHEN concept_count IS NULL THEN 0 
+        WHEN concept_count >= 5 THEN 5 
+        ELSE concept_count END AS concept_count,
+  CASE WHEN concept_01_count IS NULL THEN 0 
+        WHEN concept_01_count >= 5 THEN 5
+        ELSE concept_01_count END AS concept_01_count,
+  CASE WHEN concept_015_count IS NULL THEN 0 
+        WHEN concept_015_count >= 5 THEN 5 
+        ELSE concept_015_count END AS concept_015_count,
+  CASE WHEN concept_02_count IS NULL THEN 0 
+        WHEN concept_02_count >= 5 THEN 5 
+        ELSE concept_02_count END AS concept_02_count,
+  COUNT(query) AS query_count,
+  SUM(l.gms)/100 AS query_gms,
+FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` l
+LEFT JOIN `etsy-data-warehouse-dev.nlao.query_intent_labels_dedupped` qi 
+  ON qi.query_raw = l.query
+LEFT JOIN cte q 
+  ON q.clean_query = l.query
+GROUP BY 1,2,3,4,5
+;
+
+-- INTEREST TYPE COUNT DISTRIBUTION
+  -- QUERY BIN
+WITH cte AS (
+SELECT
+  clean_query,
+  COUNT(DISTINCT attribute_type) AS concept_type_count,
+  COUNT(DISTINCT CASE WHEN score >=0.1 THEN attribute_type ELSE NULL END) AS concept_type_01_count,
+  COUNT(DISTINCT CASE WHEN score >=0.15 THEN attribute_type ELSE NULL END) AS concept_type_015_count,
+  COUNT(DISTINCT CASE WHEN score >=0.2 THEN attribute_type ELSE NULL END) AS concept_type_02_count
+FROM `etsy-data-warehouse-dev.nlao.query_interests`
 GROUP BY 1
 )
 SELECT
   bin,
-  CASE WHEN concept_type_count IS NULL THEN 0 ELSE concept_type_count END AS concept_type_count,
-  COUNT(*) AS query_count,
+  CASE WHEN concept_type_count IS NULL THEN 0 
+        WHEN concept_type_count >= 5 THEN 5 
+        ELSE concept_type_count END AS concept_type_count,
+  CASE WHEN concept_type_01_count IS NULL THEN 0 
+        WHEN concept_type_01_count >= 5 THEN 5
+        ELSE concept_type_01_count END AS concept_type_01_count,
+  CASE WHEN concept_type_015_count IS NULL THEN 0 
+        WHEN concept_type_015_count >= 5 THEN 5 
+        ELSE concept_type_015_count END AS concept_type_015_count,
+  CASE WHEN concept_type_02_count IS NULL THEN 0 
+        WHEN concept_type_02_count >= 5 THEN 5 
+        ELSE concept_type_02_count END AS concept_type_02_count,
+  COUNT(query) AS query_count,
   SUM(l.gms)/100 AS query_gms,
 FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` l
 LEFT JOIN cte q 
-  ON q.query = l.query
-GROUP BY 1,2
+  ON q.clean_query = l.query
+GROUP BY 1,2,3,4,5
+;
+
+  -- QUERY INTENT
+WITH cte AS (
+SELECT
+  clean_query,
+  COUNT(DISTINCT attribute_type) AS concept_type_count,
+  COUNT(DISTINCT CASE WHEN score >=0.1 THEN attribute_type ELSE NULL END) AS concept_type_01_count,
+  COUNT(DISTINCT CASE WHEN score >=0.15 THEN attribute_type ELSE NULL END) AS concept_type_015_count,
+  COUNT(DISTINCT CASE WHEN score >=0.2 THEN attribute_type ELSE NULL END) AS concept_type_02_count
+FROM `etsy-data-warehouse-dev.nlao.query_interests`
+GROUP BY 1
+)
+SELECT
+  CASE WHEN confidence >= 0.5 THEN label ELSE 'Unsure' END AS label,
+  CASE WHEN concept_type_count IS NULL THEN 0 
+        WHEN concept_type_count >= 5 THEN 5 
+        ELSE concept_type_count END AS concept_type_count,
+  CASE WHEN concept_type_01_count IS NULL THEN 0 
+        WHEN concept_type_01_count >= 5 THEN 5
+        ELSE concept_type_01_count END AS concept_type_01_count,
+  CASE WHEN concept_type_015_count IS NULL THEN 0 
+        WHEN concept_type_015_count >= 5 THEN 5 
+        ELSE concept_type_015_count END AS concept_type_015_count,
+  CASE WHEN concept_type_02_count IS NULL THEN 0 
+        WHEN concept_type_02_count >= 5 THEN 5 
+        ELSE concept_type_02_count END AS concept_type_02_count,
+  COUNT(query) AS query_count,
+  SUM(l.gms)/100 AS query_gms,
+FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` l
+LEFT JOIN `etsy-data-warehouse-dev.nlao.query_intent_labels_dedupped` qi 
+  ON qi.query_raw = l.query
+LEFT JOIN cte q 
+  ON q.clean_query = l.query
+GROUP BY 1,2,3,4,5
 ;
 
 -- INTEREST TYPE
 SELECT
   attribute_type,
   display_name,
-  COUNT(DISTINCT query) AS query_count
+  COUNT(DISTINCT CASE WHEN score >=0.1 THEN attribute_type ELSE NULL END) AS concept_type_01_count,
+  COUNT(DISTINCT CASE WHEN score >=0.15 THEN attribute_type ELSE NULL END) AS concept_type_015_count,
+  COUNT(DISTINCT CASE WHEN score >=0.2 THEN attribute_type ELSE NULL END) AS concept_type_02_count
 FROM `etsy-data-warehouse-dev.nlao.query_interests`
 WHERE score >= 0.1
 GROUP BY 1,2
 ;
 
+-- POPULAR INTEREST TYPE
 SELECT
   attribute_type,
   COUNT(DISTINCT query) AS query_count
@@ -150,6 +250,8 @@ WHERE score >= 0.1
 GROUP BY 1
 ;
 
+-- POPULAR INTEREST TYPE
+  -- QUERY BIN
 SELECT
   bin,
   CASE WHEN attribute_type IS NOT NULL THEN attribute_type ELSE 'null' END AS attribute_type,
@@ -160,14 +262,160 @@ LEFT JOIN `etsy-data-warehouse-dev.nlao.query_interests` q
 GROUP BY 1,2
 ;
 
+-- POPULAR INTEREST TYPE BY THRESHOLD
+  -- 0
+WITH cte AS (
 SELECT
-  bin,
-  CASE WHEN attribute_type IS NOT NULL THEN attribute_type ELSE 'null' END AS attribute_type,
-  CASE WHEN display_name IS NOT NULL THEN display_name ELSE 'null' END AS display_name,
-  COUNT(DISTINCT l.query) AS query_count
+distinct l.query,
+attribute_type,
+gms/100 AS gms
 FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` l
-LEFT JOIN `etsy-data-warehouse-dev.nlao.query_interests` q
+JOIN `etsy-data-warehouse-dev.nlao.query_interests` q
   ON l.query = q.query
-GROUP BY 1,2,3
+) 
+SELECT 
+attribute_type,
+COUNT(query) AS query_count,
+SUM(gms) AS query_gms,
+FROM cte
+GROUP BY 1
+ORDER BY 2 DESC
 ;
+
+  -- 0.1
+WITH cte AS (
+SELECT
+distinct l.query,
+attribute_type,
+gms/100 AS gms
+FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` l
+JOIN `etsy-data-warehouse-dev.nlao.query_interests` q
+  ON l.query = q.query
+WHERE score>=0.1
+) 
+SELECT 
+attribute_type,
+COUNT(query) AS query_count,
+SUM(gms) AS query_gms,
+FROM cte
+GROUP BY 1
+ORDER BY 2 DESC
+;
+
+  -- 0.15
+WITH cte AS (
+SELECT
+distinct l.query,
+attribute_type,
+gms/100 AS gms
+FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` l
+JOIN `etsy-data-warehouse-dev.nlao.query_interests` q
+  ON l.query = q.query
+WHERE score>=0.15
+) 
+SELECT 
+attribute_type,
+COUNT(query) AS query_count,
+SUM(gms) AS query_gms,
+FROM cte
+GROUP BY 1
+ORDER BY 2 DESC
+;
+
+  -- 0.2
+WITH cte AS (
+SELECT
+distinct l.query,
+attribute_type,
+gms/100 AS gms
+FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` l
+JOIN `etsy-data-warehouse-dev.nlao.query_interests` q
+  ON l.query = q.query
+WHERE score>=0.2
+) 
+SELECT 
+attribute_type,
+COUNT(query) AS query_count,
+SUM(gms) AS query_gms,
+FROM cte
+GROUP BY 1
+ORDER BY 2 DESC
+;
+
+-- SCORE DISTRIBUTION
+SELECT
+attribute_type,
+CASE WHEN score <= 0.1 THEN '0.0-0.1'
+     WHEN score <= 0.2 THEN '0.1-0.2'
+     WHEN score <= 0.3 THEN '0.2-0.3'
+     WHEN score <= 0.4 THEN '0.3-0.4'
+     WHEN score <= 0.5 THEN '0.4-0.5'
+     WHEN score <= 0.6 THEN '0.5-0.6'
+     WHEN score <= 0.7 THEN '0.6-0.7'
+     WHEN score <= 0.8 THEN '0.7-0.8'
+     WHEN score <= 0.9 THEN '0.8-0.9'
+     ELSE '0.9-1.0' 
+     END AS score_bin,
+COUNT(*) AS predictions
+FROM `etsy-data-warehouse-dev.nlao.query_interests`
+GROUP BY 1,2
+;
+
+
+-- INTEREST TYPE OVERLAPS WITH TEXT MATCHING METHOD (HOLIDAY AND OCCASION)
+WITH cte AS (
+SELECT
+  q.query,
+  is_holiday AS text_is_holiday,
+  is_occasion AS text_is_occasion,
+  MAX(CASE WHEN score >= 0.1 AND lower(attribute_type) = "holiday" THEN 1 ELSE 0 END) AS model_is_holiday,
+  MAX(CASE WHEN score >= 0.1 AND lower(attribute_type) = "occasion" THEN 1 ELSE 0 END) AS model_is_occasion
+FROM `etsy-data-warehouse-prod.rollups.query_level_metrics` q
+LEFT JOIN `etsy-data-warehouse-dev.nlao.query_interests` qi 
+  ON q.query = qi.clean_query
+GROUP BY 1,2,3
+)
+SELECT
+model_is_holiday,
+model_is_occasion,
+text_is_holiday,
+text_is_occasion,
+COUNT(query) AS query_count
+FROM cte
+GROUP BY 1,2,3,4
+;
+
+-- CHECK INTEREST LABEL COUNT WITHIN A VISIT
+-- RANDOM DAY
+WITH cte AS (
+SELECT  
+visit_id,
+COUNT(DISTINCT query_raw) AS query_count,
+COUNT(DISTINCT CASE WHEN score >=0.1 THEN display_name ELSE NULL END) AS concept_label_01_count,
+COUNT(DISTINCT CASE WHEN score >=0.15 THEN display_name ELSE NULL END) AS concept_label_015_count,
+COUNT(DISTINCT CASE WHEN score >=0.2 THEN display_name ELSE NULL END) AS concept_label_02_count,
+FROM `etsy-data-warehouse-prod.search.query_sessions_new` q
+LEFT JOIN `etsy-data-warehouse-dev.nlao.query_interests` qi
+  ON q.query_raw = qi.clean_query
+WHERE q._date = '2022-01-02' AND query_raw IS NOT NULL
+GROUP BY 1
+)
+SELECT
+CASE WHEN query_count >=10 THEN 10 
+  ELSE query_count 
+  END AS query_count,
+CASE WHEN concept_label_01_count >= 8 THEN 8
+     WHEN concept_label_01_count BETWEEN 1 AND 7 THEN concept_label_01_count
+     ELSE 0 END AS concept_label_01_count,
+CASE WHEN concept_label_015_count >= 8 THEN 8
+     WHEN concept_label_015_count BETWEEN 1 AND 7 THEN concept_label_015_count
+     ELSE 0 END AS concept_label_015_count,
+CASE WHEN concept_label_02_count >= 8 THEN 8
+     WHEN concept_label_02_count BETWEEN 1 AND 7 THEN concept_label_02_count
+     ELSE 0 END AS concept_label_02_count,
+COUNT(visit_id) AS visit_count
+FROM cte
+GROUP BY 1,2,3,4
+;
+
 
