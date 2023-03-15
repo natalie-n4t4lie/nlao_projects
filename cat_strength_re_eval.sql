044f836af9344c17eabcc5f4428191ab7b6585b0
lao-natalie <89660903+lao-natalie@users.noreply.github.com>
2023-03-15 10:17:47 -0400

    Update cat_strength_re_eval.sql

--- cat_strength_re_eval.sql
+++ cat_strength_re_eval.sql
@@ -1,33 +1,34 @@
-BEGIN
-CREATE TEMP TABLE landing_p365 as (
+CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.nlao.landing_p365` as (
 select
+	DISTINCT
 	b._date,
 	b.visit_id,
-	b.run_date,
 	b.user_id,
-	b.visit_length,
-	b.visit_duration,
-	b.referrer_type,
-	b.referring_url,
-	b.referring_domain,
-	b.referring_keyword,
 	b.landing_event,
-	b.exit_event,
-	b.utm_campaign,
 	b.region,
 	b.landing_event_url,
-	b.top_channel,
-	b.second_channel,
-	b.third_channel,
-	b.platform,
 	case when landing_event = "market" then lower(regexp_replace(regexp_replace(regexp_substr(landing_event_url, "market/([^?]*)"), "_", " "), "\\%27", "")) else null end as landing_market_query,
 	case when landing_event like "view%listing%" then safe_cast(regexp_substr(landing_event_url, "listing\\/(\\d*)") as int64) else null end as landing_listing_id,
 	case when landing_event = "search" then regexp_replace(regexp_replace(regexp_substr(lower(landing_event_url), "q=([a-z0-9%+]+)"),"\\\\+"," "),"%20"," ") else null end as landing_search_query,
   case when landing_event = "category_page" then regexp_replace(split(regexp_substr(landing_event_url, "\\/c\\/([^\\?|\\&]+)"),"/")[safe_offset(0)],"-","_") end as landing_cat_page_top_cat,
 	case when landing_event = "category_page" then regexp_replace(split(regexp_substr(landing_event_url, "\\/c\\/([^\\?|\\&]+)"),"/")[safe_offset(1)],"-","_") end as landing_cat_page_second_cat,
-  case when landing_event = "category_page" then regexp_replace(split(regexp_substr(landing_event_url, "\\/c\\/([^\\?|\\&]+)"),"/")[safe_offset(2)],"-","_") end as landing_cat_page_third_cat
+  case when landing_event = "category_page" then regexp_replace(split(regexp_substr(landing_event_url, "\\/c\\/([^\\?|\\&]+)"),"/")[safe_offset(2)],"-","_") end as landing_cat_page_third_cat,
+	case when channel_dimensions.tactic_high_level is null then "SEO"
+  when channel_dimensions.tactic_high_level in ('Email - Marketing','Push - Marketing') then 'CRM'
+  when channel_dimensions.tactic_high_level like 'SEM%' then 'SEM'
+  when channel_dimensions.tactic_high_level like 'PLA%' then 'PLA'
+  else 'Display/Social'
+  end as channel,
 from
 	`etsy-data-warehouse-prod.weblog.visits` b
+LEFT JOIN `etsy-data-warehouse-prod.buyatt_mart.channel_dimensions` AS channel_dimensions 
+	ON lower(b.utm_campaign) = lower(channel_dimensions.utm_campaign)
+      and lower(b.utm_medium) = lower(channel_dimensions.utm_medium)
+      and lower((case when b.top_channel = 'social_promoted' then 'social_owned'
+          when b.top_channel = 'social_organic' then 'social_earned'
+          else b.top_channel end)) = lower(channel_dimensions.top_channel)
+      and lower(b.second_channel) = lower(channel_dimensions.second_channel)
+      and lower(b.third_channel) = lower(channel_dimensions.third_channel)
 where 
 	b._date >= date_sub(current_date, interval 12 month)
 	and b.platform in ("boe","desktop","mobile_web") --remove soe
@@ -38,73 +39,6 @@ where
 	AND (landing_event IN ("search","category_page","market") OR landing_event LIKE "view%listing%")
 );
 
--- H&L DEFINITION
--- Landing event is a listing in the home & listing category, or in key art & collectibles subcategories
--- Landing on a market page with a query classified in the H&L or A&C subcategory taxonomies
--- Landing on a shop whose top category is H&L (can't classified subsub category on shop level, so not include in definition)
--- Landing on a H&L or relevant A&C category page
-
-CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.nlao.hl_landing_p365` as (
-WITH hl_taxo AS (
-SELECT
-taxonomy_id,
-full_path,
-split(full_path, '.')[safe_offset(0)] as top_cat,
-split(full_path, '.')[safe_offset(1)] as second_cat,
-split(full_path, '.')[safe_offset(2)] as third_cat
-FROM `etsy-data-warehouse-prod.structured_data.taxonomy`
-WHERE split(full_path, '.')[safe_offset(0)] = "home_and_living" OR split(full_path, '.')[safe_offset(1)] in ("prints", "collectibles", "painting", "drawing_and_illustration")
-),
-hl_search AS (
-SELECT
-query,
-taxonomy_id,
-split(full_path, '.')[safe_offset(0)] as top_cat,
-split(full_path, '.')[safe_offset(1)] as second_cat,
-split(full_path, '.')[safe_offset(2)] as third_cat
-FROM `etsy-data-warehouse-prod.search.query_sessions_new` q
-JOIN hl_taxo t
-    ON q.classified_taxonomy_id = t.taxonomy_id
-WHERE q._date >= date_sub(current_date, interval 12 month)
-)
-SELECT DISTINCT
-  v._date,
-	v.visit_id,
-	v.run_date,
-	v.user_id,
-	v.visit_length,
-	v.visit_duration,
-	v.referrer_type,
-	v.referring_url,
-	v.referring_domain,
-	v.referring_keyword,
-	v.landing_event,
-	v.exit_event,
-	v.utm_campaign,
-	v.region,
-	v.landing_event_url,
-	v.top_channel,
-	v.second_channel,
-	v.third_channel,
-	v.platform,
-  COALESCE(v.landing_cat_page_top_cat,t1.top_cat,q.top_cat) AS top_cat,
-  COALESCE(v.landing_cat_page_top_cat,t1.second_cat,q.second_cat) AS second_cat,
-  COALESCE(v.landing_cat_page_third_cat,t1.third_cat,q.third_cat) AS third_cat,
-  1 AS is_hl
-FROM landing_p365 v
-LEFT JOIN hl_search q
-	ON v.landing_market_query = q.query
-LEFT JOIN `etsy-data-warehouse-prod.listing_mart.listing_attributes` a
-  ON v.landing_listing_id = a.listing_id
-LEFT JOIN hl_taxo t1
-  ON a.taxonomy_id = t1.taxonomy_id
-WHERE 
-q.taxonomy_id IS NOT NULL  -- Landing on a market page with a query classified in the H&L or A&C subcategory taxonomies
-OR t1.taxonomy_id IS NOT NULL -- Landing event is a listing in the home & listing category, or in key art & collectibles subcategories
-OR v.landing_cat_page_top_cat = "home_and_living" OR v.landing_cat_page_second_cat IN ("prints", "collectibles", "painting", "drawing_and_illustration")--  Landing on a H&L or relevant A&C category page
-)
-;
-
 -- Landing event is a listing with “gift” in the title
 -- Landing on market page where the query matches the is_gift definition outlined here
 -- Landing on an Editor’s Picks page with a title containing “gift”
@@ -112,42 +46,329 @@ CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.nlao.gifting_landing_p365` as (
 SELECT DISTINCT
   v._date,
 	v.visit_id,
-	v.run_date,
 	v.user_id,
-	v.visit_length,
-	v.visit_duration,
-	v.referrer_type,
-	v.referring_url,
-	v.referring_domain,
-	v.referring_keyword,
 	v.landing_event,
-	v.exit_event,
-	v.utm_campaign,
 	v.region,
 	v.landing_event_url,
-	v.top_channel,
-	v.second_channel,
-	v.third_channel,
-	v.platform,
+	v.channel,
   v.landing_market_query,
   v.landing_listing_id,
 	l.title,
   lower(regexp_replace(regexp_replace(regexp_substr(landing_event_url, "featured/([^?]*)"), "-", " "), "\\%27", "")) AS landing_feature_page,
 	coalesce(v.landing_market_query,l.title,lower(regexp_replace(regexp_replace(regexp_substr(landing_event_url, "featured/([^?]*)"), "-", " "), "\\%27", ""))) AS keywords,
   1 AS is_gift
-FROM landing_p365 v
+FROM `etsy-data-warehouse-dev.nlao.landing_p365` v
 LEFT JOIN `etsy-data-warehouse-prod.etsy_shard.listings` l
 	ON v.landing_listing_id = l.listing_id
 LEFT JOIN `etsy-data-warehouse-prod.rollups.query_level_metrics` q
 	ON v.landing_market_query = q.query
 WHERE 
 regexp_contains(l.title, '(\?i)\\bgift|\\bfor (\\bhim|\\bher|\\bmom|\\bdad|\\bmother|\\bfather|\\bdaughter|\\bson|\\bwife|\\bhusband|\\bpartner|\\baunt|\\buncle|\\bniece|\\bnephew|\\bfiance|\\bcousin|\\bin law|\\bboyfriend|\\bgirlfriend|\\bgrand|\\bfriend|\\bbest friend)') --Landing event is a listing with “gift” in the title
-OR
-q.is_gift = 1 -- Landing on market page where the query matches the is_gift definition outlined here
-OR
-regexp_contains(lower(regexp_replace(regexp_replace(regexp_substr(landing_event_url, "featured/([^?]*)"), "-", " "), "\\%27", "")), '(\?i)\\bgift') -- Landing on an Editor’s Picks page with a title containing “gift”
+OR q.is_gift = 1 -- Landing on market page where the query matches the is_gift definition outlined here
+OR regexp_contains(lower(regexp_replace(regexp_replace(regexp_substr(landing_event_url, "featured/([^?]*)"), "-", " "), "\\%27", "")), '(\?i)\\bgift') -- Landing on an Editor’s Picks page with a title containing “gift”
 )
 ;
 
-END
+-- H&L DEFINITION
+-- Landing event is a listing in the home & listing category, or in key art & collectibles subcategories
+-- Landing on a market page with a query classified in the H&L or A&C subcategory taxonomies
+-- Landing on a shop whose top category is H&L (can't classified subsub category on shop level, so not include in definition)
+-- Landing on a H&L or relevant A&C category page
+
+CREATE OR REPLACE TABLE `etsy-data-warehouse-dev.nlao.hl_landing_p365` as (
+WITH
+distinct_taxo as (
+	select
+	distinct 
+	taxonomy_id,
+	top_level_cat_new,
+	second_level_cat_new,
+	third_level_cat_new,
+	case when top_level_cat_new = "home_and_living" then 1
+	when second_level_cat_new in ("prints", "collectibles", "painting", "drawing_and_illustration")  then 1
+	else 0 end as is_home
+	from `etsy-data-warehouse-prod.materialized.listing_categories_taxonomy`
+)
+, query_label as (
+	select
+		s.query,
+		t.top_level_cat_new,
+		t.second_level_cat_new,
+		t.third_level_cat_new,
+		t.is_home
+	from `etsy-data-warehouse-prod.search.query_sessions_new` s
+	join distinct_taxo t 
+	on s.classified_taxonomy_id = t.taxonomy_id
+	where _date >= date_sub(current_date, interval 12 month)
+)
+SELECT 
+	v._date,
+	v.visit_id,
+	v.user_id,
+	v.landing_event,
+	v.region,
+	v.landing_event_url,
+	v.channel,
+  COALESCE(v.landing_cat_page_top_cat,t1.top_level_cat_new,q.top_level_cat_new) AS top_cat,
+  COALESCE(v.landing_cat_page_top_cat,t1.second_level_cat_new,q.second_level_cat_new) AS second_cat,
+  COALESCE(v.landing_cat_page_third_cat,t1.third_level_cat_new,q.third_level_cat_new) AS third_cat,
+  1 AS is_hl
+FROM `etsy-data-warehouse-dev.nlao.landing_p365` v
+LEFT JOIN query_label q
+	ON v.landing_market_query = q.query
+LEFT JOIN `etsy-data-warehouse-prod.listing_mart.listing_attributes` a
+  ON v.landing_listing_id = a.listing_id
+LEFT JOIN distinct_taxo t1
+  ON a.taxonomy_id = t1.taxonomy_id
+WHERE 
+q.is_home = 1 -- Landing on a market page with a query classified in the H&L or A&C subcategory taxonomies
+OR t1.is_home = 1 -- Landing event is a listing in the home & listing category, or in key art & collectibles subcategories
+OR v.landing_cat_page_top_cat = "home_and_living" OR v.landing_cat_page_second_cat IN ("prints", "collectibles", "painting", "drawing_and_illustration")--  Landing on a H&L or relevant A&C category page
+)
 ;
+
+--------------------------------------analysis-------------------------------------------------------------------------
+-- ended up using `etsy-data-warehouse-dev.awaagner.hl_gift_visits_classified` since it has most info ready
+create or replace table `etsy-data-warehouse-dev.nlao.market_data` as (
+with market as (
+	select
+	v._date,
+	v.visit_id,
+	v.converted,
+	v.bounced,
+	v.orders,
+	t.transaction_id,
+	t.listing_id,
+	t.trans_gms_net,
+  case when channel_dimensions.tactic_high_level is null then "SEO"
+  when channel_dimensions.tactic_high_level in ('Email - Marketing','Push - Marketing') then 'CRM'
+  when channel_dimensions.tactic_high_level like 'SEM%' then 'SEM'
+  when channel_dimensions.tactic_high_level like 'PLA%' then 'PLA'
+  else 'Display/Social'
+  end as channel,
+	case when tp.top_cat = "home_and_living" then "home"
+	     when tp.second_cat in ("prints", "collectibles", "painting", "drawing_and_illustration")  then "home"
+	     else tp.top_cat end as purch_vertical,
+	tp.top_cat as purch_cat,
+	tp.second_cat as purch_second_cat,
+	tp.third_cat as purch_third_cat,
+	lower(regexp_replace(regexp_replace(regexp_substr(landing_event_url, "market/([^?]*)"), "_", " "), "\\%27", "")) as query
+FROM `etsy-data-warehouse-prod.weblog.visits` v
+LEFT JOIN `etsy-data-warehouse-prod.buyatt_mart.channel_dimensions` AS channel_dimensions 
+	ON lower(v.utm_campaign) = lower(channel_dimensions.utm_campaign)
+      and lower(v.utm_medium) = lower(channel_dimensions.utm_medium)
+      and lower((case when v.top_channel = 'social_promoted' then 'social_owned'
+          when v.top_channel = 'social_organic' then 'social_earned'
+          else v.top_channel end)) = lower(channel_dimensions.top_channel)
+      and lower(v.second_channel) = lower(channel_dimensions.second_channel)
+      and lower(v.third_channel) = lower(channel_dimensions.third_channel)
+left join `etsy-data-warehouse-prod.visit_mart.visits_transactions` t
+on v.visit_id = t.visit_id
+and v._date = t._date
+left join `etsy-data-warehouse-dev.nlao.taxonomy_parsed` tp
+on t.taxonomy_id = tp.taxonomy_id
+where v._date >= date_sub(current_date, interval 12 month)
+and landing_event = "market"
+and ((converted = 1 and transaction_id is not NULL)
+or converted = 0)
+)
+, distinct_taxo as (
+	select
+	distinct 
+	taxonomy_id,
+	top_cat,
+	second_cat,
+	third_cat,
+  path as item,
+	case when top_cat = "home_and_living" then "home"
+	when second_cat in ("prints", "collectibles", "painting", "drawing_and_illustration")  then "home"
+	else top_cat end as vertical
+	from `etsy-data-warehouse-dev.nlao.taxonomy_parsed`
+)
+, query_label as (
+	select
+		s.query_raw,
+		t.top_cat,
+		t.second_cat,
+		t.third_cat,
+    t.item,
+		t.vertical,
+		count(*) as n_queries
+	from `etsy-data-warehouse-prod.search.query_sessions_new` s
+	join distinct_taxo t 
+	on s.classified_taxonomy_id = t.taxonomy_id
+	where _date >= date_sub(current_date, interval 12 month)
+	group by 1,2,3,4,5,6
+	qualify row_number() over(partition by query_raw order by count(*) desc) = 1
+)
+	select
+		visit_id,
+		query as market_page_query,
+		q.top_cat as market_top_cat,
+		q.second_cat as market_second_cat,
+		q.third_cat as market_third_cat,
+    q.item as market_item,
+		q.vertical as market_vertical,
+    m.channel,
+		-- CR
+		max(converted) as has_conversion,
+		max(case when m.purch_vertical = q.vertical then 1 else 0 end) as conversion_in_vertical,
+		max(case when m.purch_vertical = q.vertical and m.purch_second_cat = q.second_cat then 1 else 0 end) as conversion_in_subcat,
+		max(case when m.purch_vertical != q.vertical then 1 else 0 end) as conversion_in_other_vertical,
+
+
+		-- GMS
+		sum(trans_gms_net) as total_gms,
+		sum(case when m.purch_vertical = q.vertical then trans_gms_net else 0 end) as gms_in_vertical,
+		sum(case when m.purch_vertical = q.vertical and m.purch_second_cat = q.second_cat then trans_gms_net else 0 end) as gms_in_subcat,
+		sum(case when m.purch_vertical != q.vertical then trans_gms_net else 0 end) as gms_in_other_vertical
+
+	from market m
+	join query_label q
+	on m.query = q.query_raw
+	group by 1,2,3,4,5,6,7,8
+);
+
+
+-- OVERALL
+select
+  market_top_cat,
+  market_second_cat,
+  market_third_cat,
+  count(distinct visit_id) as landings
+from `etsy-data-warehouse-dev.nlao.market_data` 
+where market_vertical = "home"
+group by 1,2,3
+order by 4 desc
+;
+
+select
+  market_top_cat,
+  market_second_cat,
+  market_third_cat,
+  market_item,
+  count(distinct visit_id) as landings
+from `etsy-data-warehouse-dev.nlao.market_data` 
+where market_vertical = "home" 
+group by 1,2,3,4
+order by 5 desc
+;
+
+-- SEO
+select
+  market_top_cat,
+  market_second_cat,
+  market_third_cat,
+  count(distinct visit_id) as landings
+from `etsy-data-warehouse-dev.nlao.market_data` 
+where market_vertical = "home" AND channel = 'SEO'
+group by 1,2,3
+order by 4 desc
+;
+
+select
+  market_top_cat,
+  market_second_cat,
+  market_third_cat,
+  market_item,
+  count(distinct visit_id) as landings
+from `etsy-data-warehouse-dev.nlao.market_data` 
+where market_vertical = "home" and channel = 'SEO'
+group by 1,2,3,4
+order by 5 desc
+;
+
+-- SEM
+select
+  market_top_cat,
+  market_second_cat,
+  market_third_cat,
+  count(distinct visit_id) as landings
+from `etsy-data-warehouse-dev.nlao.market_data` 
+where market_vertical = "home" AND channel = 'SEM'
+group by 1,2,3
+order by 4 desc
+;
+
+select
+  market_top_cat,
+  market_second_cat,
+  market_third_cat,
+  market_item,
+  count(distinct visit_id) as landings
+from `etsy-data-warehouse-dev.nlao.market_data` 
+where market_vertical = "home" and channel = 'SEM'
+group by 1,2,3,4
+order by 5 desc
+;
+
+--------------------------------------------------- GIFTING------------------------------------------------------------
+-- ended up using `etsy-data-warehouse-dev.awaagner.hl_gift_visits_classified` since it has most info ready
+-- OVERALL
+SELECT
+query,
+COUNT(*) AS visits
+FROM `etsy-data-warehouse-dev.awaagner.hl_gift_visits_classified` g
+JOIN `etsy-data-warehouse-prod.search.query_sessions_new` q
+  ON g.visit_id = q.visit_id
+WHERE q._date >= date_sub(current_date, interval 12 month) 
+  AND gift_visit = 1
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10000
+;
+
+-- SEO
+SELECT
+query,
+COUNT(*) AS visits,
+FROM `etsy-data-warehouse-dev.awaagner.hl_gift_visits_classified` b
+JOIN `etsy-data-warehouse-prod.search.query_sessions_new` q
+  ON b.visit_id = q.visit_id
+LEFT JOIN `etsy-data-warehouse-prod.buyatt_mart.channel_dimensions` AS channel_dimensions 
+	ON lower(b.utm_campaign) = lower(channel_dimensions.utm_campaign)
+      and lower(b.utm_medium) = lower(channel_dimensions.utm_medium)
+      and lower((case when b.top_channel = 'social_promoted' then 'social_owned'
+          when b.top_channel = 'social_organic' then 'social_earned'
+          else b.top_channel end)) = lower(channel_dimensions.top_channel)
+      and lower(b.second_channel) = lower(channel_dimensions.second_channel)
+      and lower(b.third_channel) = lower(channel_dimensions.third_channel)
+WHERE 
+  q._date >= date_sub(current_date, interval 12 month) 
+  AND channel_dimensions.tactic_high_level is null
+  AND b.gift_visit = 1
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10000
+;
+
+-- SEM 
+SELECT
+query,
+COUNT(*) AS visits,
+FROM `etsy-data-warehouse-dev.awaagner.hl_gift_visits_classified` b
+JOIN `etsy-data-warehouse-prod.search.query_sessions_new` q
+  ON b.visit_id = q.visit_id
+LEFT JOIN `etsy-data-warehouse-prod.buyatt_mart.channel_dimensions` AS channel_dimensions 
+	ON lower(b.utm_campaign) = lower(channel_dimensions.utm_campaign)
+      and lower(b.utm_medium) = lower(channel_dimensions.utm_medium)
+      and lower((case when b.top_channel = 'social_promoted' then 'social_owned'
+          when b.top_channel = 'social_organic' then 'social_earned'
+          else b.top_channel end)) = lower(channel_dimensions.top_channel)
+      and lower(b.second_channel) = lower(channel_dimensions.second_channel)
+      and lower(b.third_channel) = lower(channel_dimensions.third_channel)
+WHERE 
+  q._date >= date_sub(current_date, interval 12 month) 
+  AND channel_dimensions.tactic_high_level like 'SEM%'
+  AND b.gift_visit = 1
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10000
+;
+
+
+
+
+
+
+
