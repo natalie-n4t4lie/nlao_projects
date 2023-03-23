9ceda7971b6f077c109294ea17d46ce7d63bddfd
lao-natalie <89660903+lao-natalie@users.noreply.github.com>
2023-03-23 12:18:26 -0400

    Update event_check.sql

--- experiment/1130656201364/event_check.sql
+++ experiment/1130656201364/event_check.sql
@@ -1,6 +1,6 @@
 -- Check event firing for [iOS] Complete the Look on Home v1 (https://atlas.etsycorp.com/catapult/1130656201364)
 
--- DELIVERED MODULE EVENT CHECK
+-- DELIVERED MODULE
 SELECT
 (select value from unnest(beacon.properties.key_value) where key = "module_placement") AS module_placement,
 count(*) AS visit_count
@@ -13,7 +13,7 @@ and (select value from unnest(beacon.properties.key_value) where key = "module_p
 group by 1
 ;-- no?
 
--- SEEN MODULE EVENT CHECK
+-- SEEN MODULE
 SELECT
 (select value from unnest(beacon.properties.key_value) where key = "module_placement") AS module_placement,
 count(*) AS visit_count
@@ -26,9 +26,9 @@ and (select value from unnest(beacon.properties.key_value) where key = "module_p
 group by 1
 ;
 
--- TAP MODULE EVENT CHECK
+-- TAP LISTING FROM MODULE 
 SELECT
-(select value from unnest(beacon.properties.key_value) where key = "module_placement") AS module_placement,
+(select value from unnest(beacon.properties.key_value) where key = "") AS module_placement,
 count(*) AS visit_count
 from
 `etsy-visit-pipe-prod.canonical.visit_id_beacons` a
@@ -38,3 +38,17 @@ and beacon.event_name = "landing_page_link_tapped"
 and (select value from unnest(beacon.properties.key_value) where key = "module_placement") like "%boe_homescreen_ctl_prior_purchase%"
 group by 1
 ;
+
+-- TAP "SEE MORE" FROM MODULE 
+SELECT
+split((select value from unnest(beacon.properties.key_value) where key = "content_source"),"-")[offset(0)] AS content_source,
+count(*) AS visit_count
+from
+`etsy-visit-pipe-prod.canonical.visit_id_beacons` a
+where
+date(_partitiontime) BETWEEN DATE_SUB(current_date, INTERVAL 14 DAY) AND CURRENT_DATE
+and beacon.event_name = "homescreen_tapped_listing"
+and (select value from unnest(beacon.properties.key_value) where key = "content_source") like "%boe_homescreen_ctl_prior_purchase%"
+group by 1
+;
+
