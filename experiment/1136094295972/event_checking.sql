86451b3651dbc36603652f5e64ec30eb4b29b428
lao-natalie <89660903+lao-natalie@users.noreply.github.com>
2023-04-12 11:58:29 -0400

    Create event_checking.sql

---
+++ experiment/1136094295972/event_checking.sql
@@ -0,0 +1,27 @@
+SELECT
+count(*) AS visit_count
+from
+`etsy-visit-pipe-prod.canonical.visit_id_beacons` a
+where
+date(_partitiontime) BETWEEN DATE_SUB(current_date, INTERVAL 14 DAY) AND CURRENT_DATE
+and beacon.event_name = "listing_screen_discover_module_seen"
+;
+
+SELECT
+count(*) AS visit_count
+from
+`etsy-visit-pipe-prod.canonical.visit_id_beacons` a
+where
+date(_partitiontime) BETWEEN DATE_SUB(current_date, INTERVAL 14 DAY) AND CURRENT_DATE
+and beacon.event_name = "listing_screen_discover_theme_tapped"
+;
+
+SELECT
+count(*) AS visit_count
+from
+`etsy-visit-pipe-prod.canonical.visit_id_beacons` a
+where
+date(_partitiontime) BETWEEN DATE_SUB(current_date, INTERVAL 14 DAY) AND CURRENT_DATE
+and beacon.event_name = "listing_screen_discover_module_delivered"
+;
+
