022b59b83ee2d9d2143763df014a2fd56540f798
lao-natalie <89660903+lao-natalie@users.noreply.github.com>
2024-05-06 12:36:16 -0400

    Update Listing views impact 'How its made' MVP.sql

--- Perso Engine/Listing views impact 'How its made' MVP.sql
+++ Perso Engine/Listing views impact 'How its made' MVP.sql
@@ -50,7 +50,7 @@ FROM labels
 )
 SELECT
 *,
-CASE WHEN is_digital = 0 AND (lower(back_end_labels) LIKE "%handmade%") THEN 1 ELSE 0 END AS show_handmade,
+CASE WHEN is_digital = 0 AND ((lower(back_end_labels) LIKE "%handmade%") OR is_handmade IN ("i_did","collective")) THEN 1 ELSE 0 END AS show_handmade,
 CASE WHEN is_vintage = 1 THEN 1 ELSE 0 END AS show_vintage,
 FROM backend_labels
 );
