ba960f6b73557ddfd33e6a373a83aa5773da067b
lao-natalie <89660903+lao-natalie@users.noreply.github.com>
2025-01-28 13:18:59 -0500

    Update Narrowing Landing Page on BOE - Product Doc.sql

--- Recommendations/Bazaar/Narrowing Landing Page on BOE - Product Doc.sql
+++ Recommendations/Bazaar/Narrowing Landing Page on BOE - Product Doc.sql
@@ -1,35 +1,31 @@
 SELECT
-case when event_source IN ('web', 'customshops', 'craft_web')
-          AND is_mobile_device = 0 THEN 'desktop'
-          when event_source IN ('web', 'customshops', 'craft_web')
-          AND is_mobile_device = 1 THEN  'mobile_web'
-          when event_source IN ('ios','android')
-          AND REGEXP_CONTAINS(LOWER(user_agent), LOWER('SellOnEtsy')) THEN 'soe'
-          when event_source IN ('ios')
-          AND NOT REGEXP_CONTAINS(LOWER(user_agent), LOWER('SellOnEtsy')) THEN 'boe ios'
-          when event_source IN ('android')
-          AND NOT REGEXP_CONTAINS(LOWER(user_agent), LOWER('SellOnEtsy')) THEN 'boe android'
-          else 'undefined' END AS app_platform,
-COUNT(DISTINCT CASE WHEN e.event_type = "boe_listing_screen_similar_listings_organic_only" THEN rv.visit_id ELSE NULL END) AS boe_listing_screen_similar_listings_organic_only,
-COUNT(DISTINCT CASE WHEN e.event_type = "view_favorites_recommendations" THEN rv.visit_id ELSE NULL END) AS view_favorites_recommendations,
-COUNT(DISTINCT CASE WHEN e.event_type = "homescreen_recent_favorites" THEN rv.visit_id ELSE NULL END) AS homescreen_recent_favorites,
-COUNT(DISTINCT CASE WHEN e.event_type = "homescreen_recently_viewed_horiz" THEN rv.visit_id ELSE NULL END) AS homescreen_recently_viewed_horiz,
-COUNT(DISTINCT CASE WHEN e.event_type = "sdl_category_page" THEN rv.visit_id ELSE NULL END) AS sdl_category_page,
-COUNT(DISTINCT CASE WHEN e.event_type = "space_page" THEN rv.visit_id ELSE NULL END) AS space_page,
-COUNT(DISTINCT CASE WHEN e.event_type = "gift_mode_gift_idea" THEN rv.visit_id ELSE NULL END) AS gift_mode_gift_idea,
-
-COUNT(CASE WHEN e.event_type = "boe_listing_screen_similar_listings_organic_only" THEN rv.visit_id ELSE NULL END) AS boe_listing_screen_similar_listings_organic_only,
-COUNT(CASE WHEN e.event_type = "view_favorites_recommendations" THEN rv.visit_id ELSE NULL END) AS view_favorites_recommendations,
-COUNT(CASE WHEN e.event_type = "homescreen_recent_favorites" THEN rv.visit_id ELSE NULL END) AS homescreen_recent_favorites,
-COUNT(CASE WHEN e.event_type = "homescreen_recently_viewed_horiz" THEN rv.visit_id ELSE NULL END) AS homescreen_recently_viewed_horiz,
-COUNT(CASE WHEN e.event_type = "sdl_category_page" THEN rv.visit_id ELSE NULL END) AS sdl_category_page,
-COUNT(CASE WHEN e.event_type = "space_page" THEN rv.visit_id ELSE NULL END) AS space_page,
-COUNT(CASE WHEN e.event_type = "gift_mode_gift_idea" THEN rv.visit_id ELSE NULL END) AS gift_mode_gift_idea,
-FROM `etsy-data-warehouse-prod.weblog.recent_visits` rv
-JOIN `etsy-data-warehouse-prod.weblog.events` e
-  ON rv.visit_id = e.visit_id
-WHERE rv._date BETWEEN CURRENT_DATE - 30 AND CURRENT_DATE
-  AND e._date BETWEEN CURRENT_DATE - 30 AND CURRENT_DATE
+ CASE WHEN beacon.event_source IN ('web', 'customshops', 'craft_web')
+          AND beacon.is_mobile_device IS FALSE THEN 'desktop'
+          when beacon.event_source IN ('web', 'customshops', 'craft_web')
+          AND beacon.is_mobile_device IS TRUE THEN  'mobile_web'
+          when beacon.event_source IN ('ios','android')
+          AND REGEXP_CONTAINS(LOWER(beacon.user_agent), LOWER('SellOnEtsy')) THEN 'soe'
+          when beacon.event_source IN ('ios')
+          AND NOT REGEXP_CONTAINS(LOWER(beacon.user_agent), LOWER('SellOnEtsy')) THEN 'boe ios'
+          when beacon.event_source IN ('android')
+          AND NOT REGEXP_CONTAINS(LOWER(beacon.user_agent), LOWER('SellOnEtsy')) THEN 'boe android'
+          else 'undefined' END AS app_platform_case,
+COUNT(DISTINCT CASE WHEN a.beacon.event_name = "boe_listing_screen_similar_listings_organic_only" THEN a.visit_id ELSE NULL END) AS boe_listing_screen_similar_listings_organic_only,
+COUNT(DISTINCT CASE WHEN a.beacon.event_name = "view_favorites_recommendations" THEN a.visit_id ELSE NULL END) AS view_favorites_recommendations,
+COUNT(DISTINCT CASE WHEN a.beacon.event_name = "homescreen_recent_favorites" THEN a.visit_id ELSE NULL END) AS homescreen_recent_favorites,
+COUNT(DISTINCT CASE WHEN a.beacon.event_name = "homescreen_recently_viewed_horiz" THEN a.visit_id ELSE NULL END) AS homescreen_recently_viewed_horiz,
+COUNT(DISTINCT CASE WHEN a.beacon.event_name = "sdl_category_page" THEN a.visit_id ELSE NULL END) AS sdl_category_page,
+COUNT(DISTINCT CASE WHEN a.beacon.event_name = "space_page" THEN a.visit_id ELSE NULL END) AS space_page,
+COUNT(DISTINCT CASE WHEN a.beacon.event_name = "gift_mode_gift_idea" THEN a.visit_id ELSE NULL END) AS gift_mode_gift_idea,
+COUNT(CASE WHEN a.beacon.event_name = "boe_listing_screen_similar_listings_organic_only" THEN a.visit_id ELSE NULL END) AS boe_listing_screen_similar_listings_organic_only,
+COUNT(CASE WHEN a.beacon.event_name = "view_favorites_recommendations" THEN a.visit_id ELSE NULL END) AS view_favorites_recommendations,
+COUNT(CASE WHEN a.beacon.event_name = "homescreen_recent_favorites" THEN a.visit_id ELSE NULL END) AS homescreen_recent_favorites,
+COUNT(CASE WHEN a.beacon.event_name = "homescreen_recently_viewed_horiz" THEN a.visit_id ELSE NULL END) AS homescreen_recently_viewed_horiz,
+COUNT(CASE WHEN a.beacon.event_name = "sdl_category_page" THEN a.visit_id ELSE NULL END) AS sdl_category_page,
+COUNT(CASE WHEN a.beacon.event_name = "space_page" THEN a.visit_id ELSE NULL END) AS space_page,
+COUNT(CASE WHEN a.beacon.event_name = "gift_mode_gift_idea" THEN a.visit_id ELSE NULL END) AS gift_mode_gift_idea,
+FROM `etsy-visit-pipe-prod.canonical.visit_id_beacons` a 
+WHERE DATE(_PARTITIONTIME) BETWEEN CURRENT_DATE - 30 AND CURRENT_DATE
 GROUP BY ALL
 ;
 
